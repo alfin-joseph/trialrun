994edfc5fbf82078eec6779f75692d0a052980b3
alfin-joseph <91449714+alfin-joseph@users.noreply.github.com>
2022-07-17 15:27:01 +0530

    deploying on heroku

--- bookinventory/bookinventory/settings.py
+++ bookinventory/bookinventory/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,8 +123,11 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [STATIC_DIR,]
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Activate Django-Heroku,
+django_heroku.settings(locals())
